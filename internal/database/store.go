7a08270c9a1f374a41340208b0f855ebde6b1032
pthum <4435508+pthum@users.noreply.github.com>
2023-06-04 13:42:42 +0200

    use any instead of interface{}

--- internal/database/store.go
+++ internal/database/store.go
@@ -11,16 +11,16 @@ import (
 )
 
 type DBReader interface {
-	GetAll(dest interface{}) error
-	Get(id string, obj interface{}) error
+	GetAll(dest any) error
+	Get(id string, obj any) error
 	Close()
 }
 
 type DBWriter interface {
-	Save(input interface{}) (err error)
-	Update(dbObject interface{}, input interface{}) (err error)
-	Create(input interface{}) (err error)
-	Delete(input interface{}) (err error)
+	Save(input any) (err error)
+	Update(dbObject any, input any) (err error)
+	Create(input any) (err error)
+	Delete(input any) (err error)
 	Close()
 }
 
@@ -74,36 +74,36 @@ func (d *GeneralDbHandler) Close() {
 }
 
 // GetAll get all objects
-func (d *GeneralDbHandler) GetAll(targets interface{}) (err error) {
+func (d *GeneralDbHandler) GetAll(targets any) (err error) {
 	return d.db.Find(targets).Error
 }
 
 // Get loads an object from the database
-func (d *GeneralDbHandler) Get(ID string, target interface{}) (err error) {
+func (d *GeneralDbHandler) Get(ID string, target any) (err error) {
 	return d.db.Where("id = ?", ID).First(&target).Error
 }
 
 // Update updates the object
-func (d *GeneralDbHandler) Update(dbObject interface{}, input interface{}) (err error) {
+func (d *GeneralDbHandler) Update(dbObject any, input any) (err error) {
 	// calculate the difference, as gorm seem to update too much fields
 	fields := findPartialUpdateFields(dbObject, input)
 	return d.db.Model(dbObject).Debug().Select(fields).Updates(input).Error
 }
 
-func (d *GeneralDbHandler) Create(input interface{}) (err error) {
+func (d *GeneralDbHandler) Create(input any) (err error) {
 	return d.db.Create(input).Error
 }
 
-func (d *GeneralDbHandler) Save(input interface{}) (err error) {
+func (d *GeneralDbHandler) Save(input any) (err error) {
 	return d.db.Debug().Save(input).Error
 }
 
-func (d *GeneralDbHandler) Delete(input interface{}) (err error) {
+func (d *GeneralDbHandler) Delete(input any) (err error) {
 	return d.db.Delete(input).Error
 }
 
 // findPartialUpdateFields find the fields that need to be updated
-func findPartialUpdateFields(dbObject interface{}, input interface{}) (fields []string) {
+func findPartialUpdateFields(dbObject any, input any) (fields []string) {
 	tIn := reflect.TypeOf(input)
 	tDb := reflect.TypeOf(dbObject)
 	if tIn.Kind() != tDb.Kind() || tIn != tDb {
