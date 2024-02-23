5fb73f615e82855197abddbdb63cc414682d3f8a
pthum <4435508+pthum@users.noreply.github.com>
2024-02-23 23:26:24 +0100

    Revert "enable debug output in scripts"
    This reverts commit 2b3daf7af02f0806ec0ee9597f3a5ba15b9c85ee.

--- build/testentrypoint.sh
+++ build/testentrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -x
 runtests() {
   go test -coverprofile=./cov.out -json ./... 2>&1 | tee | go-junit-report -parser gojson > report.xml \
     && gocover-cobertura < cov.out > coverage.xml
